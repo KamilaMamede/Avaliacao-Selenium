68849206ffdf650f1f77a71a97bf0c1f9a82cf37
KamilaMamede <kamilamamede1@outlook.com>
2023-06-23 07:55:41 -0300

    tentando localizar o elemento para aceitar os cookies

--- src/test/java/br/com/nttdata/avaliacaoselenium/AvaliacaoSeleniumTest.java
+++ src/test/java/br/com/nttdata/avaliacaoselenium/AvaliacaoSeleniumTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,18 +16,18 @@ public class AvaliacaoSeleniumTest {
     public void pesquisaVaga() {
         System.setProperty("webdriver.chrome.driver", "/C:/Users/kandress/OneDrive - NTT DATA EMEAL/Documentos/Curso de Automação/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("debuggerAddress", "localhost:9222");
         browser.navigate().to("https://www.google.com.br/");
         browser.findElement(By.cssSelector("textarea.gLFyf")).clear();
         browser.findElement(By.cssSelector("textarea.gLFyf")).sendKeys("NTT DATA");
         browser.findElement(By.cssSelector("textarea.gLFyf")).sendKeys(Keys.RETURN);
         browser.findElement(By.cssSelector("h3.LC20lb.MBeuO.DKV0Md")).click();
-        browser.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-        //browser.findElement(By.xpath("//*[@id=\"grouped-pageload-Banner\"]/div/div/div/div[3]/button[2]")).click();
+        browser.manage().timeouts().implicitlyWait(8,TimeUnit.SECONDS);
+        browser.findElement(By.xpath("// button[@class=\"intro-banner-btn save-consents evSpAcceptBtn\"]")).click();
         //browser.findElement(By.cssSelector(".navbar-link.has-children.text-hidden, .navbar-link.is-search.text-hidden")).click();
-        browser.findElement(By.cssSelector(".navbar-list.level-0>.navbar-list-item>.navbar-link:not(.btn):before")).click();
+        //browser.findElement(By.className("intro-banner-btn save-consents evSpAcceptBtn")).click();
 
-       /* browser.navigate().to("https://br.nttdata.com/");
-        browser.findElement(By.cssSelector("navbar-link is-active")).click();*/
 
     }
 
